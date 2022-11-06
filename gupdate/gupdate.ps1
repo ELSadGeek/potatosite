826b80e5be12dcb01b1d5d1298f1f298b3425289
ElSadGeek <geekmode0n@outlook.com>
2022-11-05 19:02:57 -0600

    fetch? latest error

--- gupdate/gupdate.ps1
+++ gupdate/gupdate.ps1
@@ -13,6 +13,7 @@ Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
 #Update potatosite repo
 Set-Location -Path "C:\Program Files\MTI\potatosite" -PassThru
 git pull origin main
+git fetch
 
 choco 7zip.install adobereader chocolateygui google-drive-file-stream googlechrome greenshot paint.net sysinternals ultravnc vlc libreoffice-fresh microsoft-teams.install zoom webex-meetings naps2 --ignore-checksums
 
