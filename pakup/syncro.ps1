002ab68473feb18b4398ac5d5fb5274c456d563f
ElSadGeek <geekmode0n@outlook.com>
2022-11-08 22:29:37 -0600

    Create syncro.ps1

---
+++ pakup/syncro.ps1
@@ -0,0 +1,21 @@
+#RemoteBackup
+
+
+#LocalDirectories
+
+$sys_path="C:\sysmti\"
+$sync="D:\systemas\sysmti\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
+
+$sys_path="C:\sysmerc\"
+$sync="D:\systemas\sysmerc\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
+
+$sys_path="C:\sysmti\"
+$sync="D:\systemas\sysmti\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
+
+$sys_path="D:"
+$sync="B:\RDS\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
+
