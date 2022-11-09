dc456106d25acff50962b4d9a5f8de5f7f3a01ec
ElSadGeek <geekmode0n@outlook.com>
2022-11-09 12:42:31 -0600

    Update syncro_aspel.ps1

--- syncro_aspel.ps1
+++ syncro_aspel.ps1
@@ -1,7 +1,7 @@
-$sys_path="C:\Program Files (x86)\Common Files\Aspel\"
-$sync="D:\ASPEL\dacaspel"
+$sys_path='C:\Program Files (x86)\Common Files\Aspel'
+$sync='D:\ASPEL\dacaspel'
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
 
 $sys_path="D:\ASPEL"
-$sync="B:\ASPEL\"
-robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
+$sync="B:\ASPEL"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN"
