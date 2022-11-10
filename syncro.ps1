4c819250c7af30101a85724a2975d736045a11ff
ElSadGeek <geekmode0n@outlook.com>
2022-11-09 22:09:17 -0600

    Update syncro.ps1

--- syncro.ps1
+++ syncro.ps1
@@ -3,21 +3,21 @@
 
 # #LocalDirectories
 
-# $sys_path="C:\sysmti\"
-# $sync="D:\systemas\sysmti\"
-# robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
+$sys_path="C:\sysmti\"
+$sync="D:\systemas\sysmti\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
 
-# $sys_path="C:\sysmerc\"
-# $sync="D:\systemas\sysmerc\"
-# robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
+$sys_path="C:\sysmerc\"
+$sync="D:\systemas\sysmerc\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
 
-# $sys_path="C:\sysmti\"
-# $sync="D:\systemas\sysmti\"
-# robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
+$sys_path="C:\sysmti\"
+$sync="D:\systemas\sysmti\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
 
-# $sys_path="D:"
-# $sync="B:\RDS\"
-# robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
+$sys_path="D:"
+$sync="B:\RDS\"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
 
 
 $sys_path='C:\sysmti\comprobantes'
