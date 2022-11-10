301f06af1dd9ea9e9531af88573138c48f5283ff
ElSadGeek <geekmode0n@outlook.com>
2022-11-10 14:59:58 -0600

    Update syncro.ps1

--- syncro.ps1
+++ syncro.ps1
@@ -19,6 +19,14 @@ $sys_path='C:\sysmti\comprobantes'
 $sync='D:\comprobantes\sysmti'
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
 
+$sys_path='C:\sysmerc\comprobantes'
+$sync='D:\comprobantes\sysmerc'
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
+
+$sys_path='C:\sysamaya\comprobantes'
+$sync='D:\comprobantes\sysamaya'
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
+
 $sys_path="D:"
 $sync="B:\RDS\"
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "System Volume Information" "$RECYCLE.BIN"
