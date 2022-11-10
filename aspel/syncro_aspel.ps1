74c5e524ada762351c246d8757063b63291dd803
ElSadGeek <geekmode0n@outlook.com>
2022-11-10 16:40:29 -0600

    '''''' ¿¿¿¿

--- aspel/syncro_aspel.ps1
+++ aspel/syncro_aspel.ps1
@@ -23,7 +23,7 @@ robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
 
 
 $sys_path="Q:\sysmti"
-$sync="G:\Unidades compartidas\COMPROBANTES"
+$sync="G:\'Unidades compartidas'\COMPROBANTES"
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
 
 
