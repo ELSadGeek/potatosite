701dec652e195ea0bfe4e6448638627c2b8cd186
ElSadGeek <geekmode0n@outlook.com>
2022-11-10 14:01:55 -0600

    sin V

--- aspel/syncro_aspel.ps1
+++ aspel/syncro_aspel.ps1
@@ -2,10 +2,13 @@ $sys_path='C:\Program Files (x86)\Common Files\Aspel'
 $sync='D:\ASPEL\dacaspel'
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
 
-$sys_path="D:\ASPEL"
-$sync="B:\ASPEL"
-robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN".
+
 
 $sys_path = "Z:\comprobantes"
 $sync ="B:\ASPEL"
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN".
+
+
+# $sys_path="D:\ASPEL"
+# $sync="B:\ASPEL"
+# robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN".
