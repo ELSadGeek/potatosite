68ee28fd4f808458d7fa5c02f526239196c52d7a
ElSadGeek <geekmode0n@outlook.com>
2022-11-10 14:22:26 -0600

    moreee comprovantz

--- aspel/syncro_aspel.ps1
+++ aspel/syncro_aspel.ps1
@@ -1,16 +1,17 @@
 $sys_path='C:\Program Files (x86)\Common Files\Aspel'
 $sync='D:\ASPEL\dacaspel'
-robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*"
-
-
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "*temp*" "*.ctl"
 
 $sys_path = "Z:\comprobantes"
 $sync ="D:\comprobantes"
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN".
 
-
-$sys_path = "D:\comprobantes"
+$sys_path = "Z:\comprobantes"
 $sync ="G:\Unidades compartidas\COMPROBANTES"
+robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN".
+
+$sys_path = "Z:\comprobantes"
+$sync ="B:\comprobantes"
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XD "$RECYCLE.BIN".
 
 
