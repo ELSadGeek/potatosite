52d78edb97506debf7032ff27c28e873bd8d0f4d
ElSadGeek <geekmode0n@outlook.com>
2022-11-10 16:02:27 -0600

    Update syncro_aspel.ps1

--- aspel/syncro_aspel.ps1
+++ aspel/syncro_aspel.ps1
@@ -17,13 +17,14 @@ robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
 # $sync ="G:\Unidades compartidas\COMPROBANTES"
 # robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
 
-$sys_path="D:\ASPEL"
-$sync="B:\ASPEL"
-robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
-
-
 $sys_path='Q:\sysmti'
 $sync='G:\Unidades compartidas\COMPROBANTES'
 robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
+
+
+# $sys_path="D:\ASPEL"
+# $sync="B:\ASPEL"
+# robocopy "$sys_path" "$sync" /R:5 /W:5 /MT:16 /MIR /Z /XF $XF XD/ $XD
+
 
 # filemon 5US*!qQcxunC8%5
