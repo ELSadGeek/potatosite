1946960898652a2e73b755f22c8548d0db3c10ec
ElSadGeek <geekmode0n@outlook.com>
2022-11-30 09:38:06 -0600

    testing
    test IT user

--- install.ps1
+++ install.ps1
@@ -79,4 +79,37 @@ $taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
 # Update the task principal settings
 Set-ScheduledTask -TaskName $taskName -Principal $taskPrincipal -Settings $taskSettings 
 
+#Task Schedule
+
+$taskName = "gupdate"
+# Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
+# Describe the scheduled task.
+$description = "GUPDATE AGENT"
+
+# Create a new task action
+
+$taskAction = New-ScheduledTaskAction `
+    -Execute 'powershell.exe' `
+    -Argument '-File "C:\Program Files\MTI\potatosite\gupdate\gupdate.ps1"'
+
+$taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10)
+
+
+# Register the scheduled task
+
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
+    -Description $description
+
+
+    $UserId = "IT"
+# Set the task principal's user ID and run level.
+$taskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -RunLevel Highest 
+# Set the task compatibility value to Windows 10.
+$taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+# Update the task principal settings
+Set-ScheduledTask -TaskName $taskName -Principal $taskPrincipal -Settings $taskSettings 
+
 Restart-Computer
--- load_potato.ps1
+++ load_potato.ps1
@@ -18,38 +18,6 @@ Import-Module .\HardeningKitty.psm1
 Invoke-HardeningKitty -EmojiSupport
 
 
-#Task Schedule
-
-$taskName = "gupdate"
-# Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
-# Describe the scheduled task.
-$description = "GUPDATE AGENT"
-
-# Create a new task action
-
-$taskAction = New-ScheduledTaskAction `
-    -Execute 'powershell.exe' `
-    -Argument '-File "C:\Program Files\MTI\potatosite\gupdate\gupdate.ps1"'
-
-$taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 10)
-
-# Register the scheduled task
-
-Register-ScheduledTask `
-    -TaskName $taskName `
-    -Action $taskAction `
-    -Trigger $taskTrigger `
-    -Description $description
-
-
-    $UserId = "IT"
-# Set the task principal's user ID and run level.
-$taskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -RunLevel Highest 
-# Set the task compatibility value to Windows 10.
-$taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
-# Update the task principal settings
-Set-ScheduledTask -TaskName $taskName -Principal $taskPrincipal -Settings $taskSettings 
-
 Unregister-ScheduledTask -TaskName "load_potato" -Confirm:$false
 
 Restart-Computer
